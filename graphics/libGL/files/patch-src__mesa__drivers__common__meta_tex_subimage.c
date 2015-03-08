6fd588ec552dfc138fb208be2296ea57dff26c15
Koop Mast <kwm@rainbow-runner.nl>
2015-03-08 21:50:48 +0100

    update patch to actualy cherry picked version to upstream 10.5 branch.

--- graphics/libGL/files/patch-src__mesa__drivers__common__meta_tex_subimage.c
+++ graphics/libGL/files/patch-src__mesa__drivers__common__meta_tex_subimage.c
@@ -1,4 +1,4 @@
-From b1ab02d9c0cc11ba8ef4efaba9452d644b6a0811 Mon Sep 17 00:00:00 2001
+From 53d66c0c7c39fbe2a587a849e0714d6f9a01f476 Mon Sep 17 00:00:00 2001
 From: Jason Ekstrand <jason.ekstrand@intel.com>
 Date: Fri, 27 Feb 2015 12:29:03 -0800
 Subject: meta/TexSubImage: Stash everything other than PIXEL_TRANSFER/store in
@@ -12,12 +12,13 @@ will get saved/restored and can't affect the texture download.
 Cc: 10.5 <mesa-stable@lists.freedesktop.org>
 Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=89292
 Reviewed-by: Neil Roberts <neil@linux.intel.com>
+(cherry picked from commit b1ab02d9c0cc11ba8ef4efaba9452d644b6a0811)
 
 diff --git a/src/mesa/drivers/common/meta_tex_subimage.c b/src/mesa/drivers/common/meta_tex_subimage.c
-index bba2b4f..971ed59 100644
+index ffbbacd..9f0c115 100644
 --- src/mesa/drivers/common/meta_tex_subimage.c
 +++ src/mesa/drivers/common/meta_tex_subimage.c
-@@ -193,8 +193,8 @@ _mesa_meta_pbo_TexSubImage(struct gl_context *ctx, GLuint dims,
+@@ -183,8 +183,8 @@ _mesa_meta_pbo_TexSubImage(struct gl_context *ctx, GLuint dims,
     if (allocate_storage)
        ctx->Driver.AllocTextureImageBuffer(ctx, tex_image);
  
@@ -28,7 +29,7 @@ index bba2b4f..971ed59 100644
  
     _mesa_GenFramebuffers(2, fbos);
     _mesa_BindFramebuffer(GL_READ_FRAMEBUFFER, fbos[0]);
-@@ -312,8 +312,8 @@ _mesa_meta_pbo_GetTexSubImage(struct gl_context *ctx, GLuint dims,
+@@ -304,8 +304,8 @@ _mesa_meta_pbo_GetTexSubImage(struct gl_context *ctx, GLuint dims,
     if (!pbo_tex_image)
        return false;
  
