4a8173b4f7bc7ed9efd0270d1c883d614160362b
Koop Mast <kwm@rainbow-runner.nl>
2015-11-18 23:54:01 +0100

    Fix a uninitialized variable, found by clang.

--- x11-servers/xorg-server/files/patch-hw_xfree86_common_xf86Xinput.c
+++ x11-servers/xorg-server/files/patch-hw_xfree86_common_xf86Xinput.c
@@ -1,5 +1,15 @@
-Index: hw/xfree86/common/xf86Xinput.c
-@@ -942,6 +942,7 @@
+--- hw/xfree86/common/xf86Xinput.c.orig	2015-10-28 19:15:36.000000000 +0100
++++ hw/xfree86/common/xf86Xinput.c	2015-11-18 23:51:11.993397000 +0100
+@@ -841,7 +841,7 @@ xf86NewInputDevice(InputInfoPtr pInfo, D
+ {
+     InputDriverPtr drv = NULL;
+     DeviceIntPtr dev = NULL;
+-    Bool paused;
++    Bool paused = 0;
+     int rval;
+     const char *path;
+ 
+@@ -993,6 +993,7 @@ NewInputDeviceRequest(InputOption *optio
          if (strcmp(key, "_source") == 0 &&
              (strcmp(value, "server/hal") == 0 ||
               strcmp(value, "server/udev") == 0 ||
