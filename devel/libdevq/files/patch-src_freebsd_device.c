0cd737324ec2b9e6a17a9f71e51274980da90a4b
Koop Mast <kwm@rainbow-runner.nl>
2016-07-28 12:36:17 +0200

    Really fix the devq_device_get_pciid_from_fd() function. Thanks dumbbell@!

--- devel/libdevq/files/patch-src_freebsd_device.c
+++ devel/libdevq/files/patch-src_freebsd_device.c
@@ -1,5 +1,5 @@
 --- src/freebsd/device.c.orig	2016-07-09 18:40:13.000000000 +0200
-+++ src/freebsd/device.c	2016-07-27 23:06:49.209320000 +0200
++++ src/freebsd/device.c	2016-07-28 12:31:40.656295000 +0200
 @@ -401,9 +401,9 @@ devq_device_get_pciid_full_from_fd(int f
  	if (ret != 0)
  		return (-1);
@@ -12,13 +12,17 @@
  		errno = EINVAL;
  		return (-1);
  	}
-@@ -421,7 +421,7 @@ devq_device_get_pciid_from_fd(int fd,
- 	int *subvendor_id, *subdevice_id, *revision_id;
+@@ -418,10 +418,10 @@ int
+ devq_device_get_pciid_from_fd(int fd,
+     int *vendor_id, int *device_id)
+ {
+-	int *subvendor_id, *subdevice_id, *revision_id;
++	int subvendor_id, subdevice_id, revision_id;
  
  	return devq_device_get_pciid_full_from_fd(fd,
 -		vendor_id, device_id, subvendor_id,
 -		subdevice_id, revision_id);
-+		&vendor_id, &device_id, &subvendor_id,
++		vendor_id, device_id, &subvendor_id,
 +		&subdevice_id, &revision_id);
  }
  
