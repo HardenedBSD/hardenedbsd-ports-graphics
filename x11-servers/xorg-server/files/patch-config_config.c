688bf20fe147b26f89a09bb09a4d4928e2dea983
Johannes Lundberg <yohanesu75@gmail.com>
2016-08-23 14:09:56 -0700

    Wayland ports updates.
    Added Wayland backend option to GTK30.
    Added GLESv2 option to cairo and made that default backend.
    Various fixes to Wayland ports.
    Enabled DRI3.

--- x11-servers/xorg-server/files/patch-config_config.c
+++ x11-servers/xorg-server/files/patch-config_config.c
@@ -1,6 +1,6 @@
---- config/config.c.orig	2014-04-12 15:29:14 UTC
+--- config/config.c.orig	2016-07-19 17:07:29 UTC
 +++ config/config.c
-@@ -64,6 +64,9 @@ config_init(void)
+@@ -55,6 +55,9 @@ config_init(void)
  #elif defined(CONFIG_WSCONS)
      if (!config_wscons_init())
          ErrorF("[config] failed to initialise wscons\n");
@@ -10,8 +10,8 @@
  #endif
  }
  
-@@ -82,6 +85,8 @@ config_fini(void)
-     config_dbus_core_fini();
+@@ -67,6 +70,8 @@ config_fini(void)
+     config_hal_fini();
  #elif defined(CONFIG_WSCONS)
      config_wscons_fini();
 +#elif defined(CONFIG_DEVD)
